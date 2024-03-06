3568705de3296bbb3bf353c183f75099ac43e64d
Shashank Daima <daimashashank10@gmail.com>
2024-03-07 01:55:15 +0530

    minor

--- src/config.py
+++ src/config.py
@@ -3,3 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 CRACKED_DEV_API_KEY = os.environ.get("CRACKED_DEV_API_KEY")
+CRACKED_DEV_CREDS=[os.environ.get("CRACKED_DEV_EMAIL"),os.environ.get("CRACKED_DEV_PASSWORD")]
--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 from .workers.cracked_dev_api import CrackedDevApi
-
-# from webdriver_manager import WebDriver
+import time
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-
-
-def main(driver: WebDriver):
-    crackedDevJobs = CrackedDevApi.get_cracked_dev_jobs(pageNo=1, pageSize=12)
+def main(driver:WebDriver):
+    crackedDevJobs= CrackedDevApi.get_cracked_dev_jobs(pageNo=1, pageSize=12);
+    # for i in range(len(crackedDevJobs)):
     driver.get(crackedDevJobs[0]["apply_url"])
-    # print(driver.title)
-    driver.find_element(
-        by=By.XPATH,
-        value="/html/body/div[2]/section/div[2]/div/div/aside/div[1]/div/div[3]/a",
-    )
-    while True:
-        a = 0
+    time.sleep(2)
+    url_to_job=(driver.find_element(by=By.XPATH,value= "/html/body/div[2]/section/div[2]/div/div/aside/div[1]/div/div[3]/a").get_attribute("href"))
+    driver.get(url=url_to_job)
+    time.sleep(1)
+    driver.execute_script("window.scrollBy(0, 200)")
+    while(True):
+        a=0;
+    
